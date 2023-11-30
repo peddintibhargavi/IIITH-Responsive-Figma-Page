0537b014aa5a31fae407d726a876b9c77636b297
peddintibhargavi <96357922+peddintibhargavi@users.noreply.github.com>
2023-11-30 18:50:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import Elements from './components/Head&Foot/Elements';
+import Tech from './components/Logos/Logos';
+import Main from './components/Community/Community';
+import Footer from './components/Details/Details';
+import Cards from './components/Cards/Cards';
+
+function App() {
+  return (
+    <>
+    <div className='main'>
+      <Elements/>
+    <Tech/>
+   <Main/>
+    <Footer/>
+    <Cards/>
+    </div>
+    </>
+  );
+}
+
+export default App;
